cd9ff9a7ce17637d3b53be967235277ea90c8f34
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-03 21:07:49 +0900

    fix: request login url 수정

--- frontend/src/components/home/LoginButton.tsx
+++ frontend/src/components/home/LoginButton.tsx
@@ -14,11 +14,11 @@ const LoginButton = ({
   const handleLogin = () => {
     const isLocal = import.meta.env.VITE_IS_LOCAL === "true";
     const state = isLocal ? "1004" : null;
-    window.location.href = `${
+    const baseUrl = `${
       import.meta.env.VITE_API_BASE_URL
-    }/api/v1/auth/${provider}/login/${isCustomer ? `customer` : "seller"}${
-      state ? `?state=${state}` : ""
-    }`;
+    }/api/v1/auth/${provider}/login/${isCustomer ? `customer` : "seller"}`;
+    const url = state ? `${baseUrl}?state=${state}` : baseUrl;
+    window.location.href = url;
   };
 
   return (
