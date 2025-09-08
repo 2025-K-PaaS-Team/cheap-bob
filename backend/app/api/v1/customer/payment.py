d8d1b0932172bf683569b0737d59795001cbd39c
hyeonsang010716 <kyr76789@gmail.com>
2025-09-08 13:52:55 +0900

    fix: cart_item에서 order_item으로 갈 때, created_at -> reservation_at으로 수정

--- backend/app/api/v1/customer/payment.py
+++ backend/app/api/v1/customer/payment.py
@@ -308,7 +308,7 @@ async def confirm_payment(
             "quantity": cart_item.quantity,
             "price": cart_item.price,
             "status": OrderStatus.reservation,
-            "created_at": datetime.now(timezone.utc)
+            "reservation_at": datetime.now(timezone.utc)
         }
         
         await order_repo.create(**order_data)
