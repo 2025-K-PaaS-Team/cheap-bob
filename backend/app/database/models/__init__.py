c49a7ac622d87695062e9675cb8dee6482cd888a
hyeonsang010716 <kyr76789@gmail.com>
2025-09-13 16:44:44 +0900

    refactor: 이전 버전 가게 위치 데이터베이스 코드 삭제

--- backend/app/database/models/__init__.py
+++ backend/app/database/models/__init__.py
@@ -2,7 +2,6 @@ from database.session import Base
 from database.models.customer import Customer
 from database.models.seller import Seller
 from database.models.store import Store
-# from database.models.store_location import StoreLocation # 추후 개발 예정
 from database.models.store_payment_info import StorePaymentInfo
 from database.models.store_product_info import StoreProductInfo
 from database.models.cart_item import CartItem
@@ -16,7 +15,6 @@ __all__ = [
     "Customer",
     "Seller",
     "Store",
-    # "StoreLocation",
     "StorePaymentInfo",
     "StoreProductInfo",
     "CartItem",
