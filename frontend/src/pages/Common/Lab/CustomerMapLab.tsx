74213f50cf76165e0b4d5d36e5f309ad60afc6dd
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-15 15:26:56 +0900

    feat: 매장 위치 등록 실험실 구현

--- frontend/src/pages/Common/Lab/CustomerMapLab.tsx
+++ frontend/src/pages/Common/Lab/CustomerMapLab.tsx
@@ -11,6 +11,7 @@ const CustomerMapLab = () => {
     endLng: 126.8355968,
   };
   const directionUrl = `http://map.naver.com/index.nhn?slng=${startCoord.lng}&slat=${startCoord.lat}&stext=내위치&elng=${endCoord.endLng}&elat=${endCoord.endLat}&etext=도착가게&menu=route&pathType=1`;
+
   const handleClickDirection = () => {
     if (!startCoord) return;
     window.open(directionUrl, "_blank");
@@ -30,59 +31,6 @@ const CustomerMapLab = () => {
         console.warn(`ERROR(${err.code}): ${err.message}`);
       }
     );
-
-    // Get naver map
-    let map: any;
-
-    const loadScript = () => {
-      return new Promise<void>((resolve) => {
-        const script = document.createElement("script");
-        script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${
-          import.meta.env.VITE_NAVER_MAP_CLIENT_ID
-        }`;
-        script.async = true;
-        script.onload = () => resolve();
-        document.body.appendChild(script);
-      });
-    };
-
-    const initMap = () => {
-      map = new window.naver.maps.Map("map", {
-        center: new window.naver.maps.LatLng(37.5666805, 126.9784147),
-        zoom: 10,
-        mapTypeId: window.naver.maps.MapTypeId.NORMAL,
-      });
-    };
-
-    const setCurrentLocation = () => {
-      if (!navigator.geolocation) return;
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          const crd = pos.coords;
-          const location = new window.naver.maps.LatLng(
-            crd.latitude,
-            crd.longitude
-          );
-          map.setCenter(location);
-          map.setZoom(10);
-        },
-        (err) => {
-          console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
-      );
-    };
-
-    loadScript().then(() => {
-      initMap();
-      setCurrentLocation();
-    });
-
-    return () => {
-      const scripts = document.querySelectorAll(
-        'script[src*="oapi.map.naver.com"]'
-      );
-      scripts.forEach((s) => s.remove());
-    };
   }, []);
 
   return (
@@ -92,7 +40,6 @@ const CustomerMapLab = () => {
         alt="directionIcon"
         onClick={handleClickDirection}
       />
-      <div id="map" style={{ width: "100%", height: "400px" }}></div>;
     </>
   );
 };
--- frontend/src/pages/Common/Lab/SellerMapLab.tsx
+++ frontend/src/pages/Common/Lab/SellerMapLab.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from "react";
 
 const SellerMapLab = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<any>(null);
 
+  // load postal code
   const loadDaumPostcode = () => {
     return new Promise<void>((resolve) => {
       if (window.daum && window.daum.Postcode) {
@@ -20,16 +22,26 @@ const SellerMapLab = () => {
     });
   };
 
+  // 좌표 불러오기 + 네이버 지도 이동
   const handleGetCoordinate = async (addr: string) => {
     const res = await getCoordinate(addr);
     alert(
       `빌딩 이름: ${res.building_name} 위도: ${res.lat} 경도: ${res.lng} 도로명주소: ${res.road_address}`
     );
+    if (mapRef.current) {
+      const newCenter = new window.naver.maps.LatLng(res.lat, res.lng);
+      alert(newCenter);
+      mapRef.current.setCenter(newCenter);
+      mapRef.current.setZoom(16, true);
+    } else {
+      console.warn("mapRef가 존재하지 않습니다");
+    }
 
     return res;
   };
 
   useEffect(() => {
+    // Get kakao postal code
     loadDaumPostcode().then(() => {
       if (
         containerRef.current &&
@@ -48,12 +60,45 @@ const SellerMapLab = () => {
         });
       }
     });
+
+    // Get naver map
+    const loadScript = () => {
+      return new Promise<void>((resolve) => {
+        const script = document.createElement("script");
+        script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${
+          import.meta.env.VITE_NAVER_MAP_CLIENT_ID
+        }`;
+        script.async = true;
+        script.onload = () => resolve();
+        document.body.appendChild(script);
+      });
+    };
+
+    const initMap = () => {
+      mapRef.current = new window.naver.maps.Map("map", {
+        center: new window.naver.maps.LatLng(37.5666805, 126.9784147),
+        zoom: 10,
+        mapTypeId: window.naver.maps.MapTypeId.NORMAL,
+      });
+    };
+
+    loadScript().then(() => {
+      initMap();
+    });
+
+    return () => {
+      const scripts = document.querySelectorAll(
+        'script[src*="oapi.map.naver.com"]'
+      );
+      scripts.forEach((s) => s.remove());
+    };
   }, []);
 
   return (
     <>
-      <h1>Postal Code</h1>
-      <div ref={containerRef} className="w-full h-[400px]"></div>
+      <h1>매장 위치 등록 실험실</h1>
+      <div ref={containerRef} className="w-full h-[500px] overflow-y-auto" />
+      <div id="map" style={{ width: "100%", height: "250px" }} />;
     </>
   );
 };
