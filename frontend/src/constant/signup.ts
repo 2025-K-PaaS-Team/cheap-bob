d3414c96931d6b44721a253129e70e26e6cf4586
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-16 10:55:38 +0900

    feat: signup 상수 객체 정의

--- frontend/src/constant/signup.ts
+++ frontend/src/constant/signup.ts
@@ -1,6 +1,61 @@
-export const NutritionGoalList = {
+export const NutritionList = {
   diet: {
     title: "다이어트",
     desc: "체지방 감소와 활력 증진",
   },
+  LCHF: {
+    title: "저탄고지",
+    desc: "식곤증 없는 안정적 에너지",
+  },
+  protein: {
+    title: "단백질 보충",
+    desc: "근육 성장과 체력 강화",
+  },
+  LSLS: {
+    title: "저당저염",
+    desc: "만성질환 예방과 저속노화",
+  },
+  balance: {
+    title: "균형잡힌",
+    desc: "영양 충전으로 활기차게",
+  },
+  vegetarian: {
+    title: "채식 위주",
+    desc: "심장 건강 개선과 체중감량",
+  },
+};
+
+export const MenuList = {
+  salad: "샐러드",
+  poke: "포케",
+  korean: "도시락",
+  sandwich: "샌드위치",
+};
+
+export const ToppingList = {
+  egg_mayo: "에그마요",
+  chicken_breast: "닭가슴살",
+  onion_flake: "어니언 후레이크",
+  toasted_bread: "구운 식빵",
+  sweet_pumpkin: "단호박",
+  ricotta_cheese: "리코타치즈",
+  shrimp: "새우",
+  smoked_salmon: "훈제연어",
+  avocado: "아보카도",
+  pork_belly: "우삼겹",
+};
+
+export const AllergyList = {
+  seafood: "해산물",
+  peanut: "땅콩",
+  nuts: "견과류",
+  soy: "대두",
+  wheat: "밀",
+  egg: "계란",
+  dairy: "유제품",
+  shellfish: "갑각류",
+  fish: "어패류",
+  pork: "돼지고기",
+  beef: "쇠고기",
+  chicken: "닭고기",
 };
