95fc8a9dd57ac65ea3f3c916fb0f937887dbcf08
hyeonsang010716 <kyr76789@gmail.com>
2025-09-16 17:14:40 +0900

    refactor: register Router 위치 변경

--- backend/app/api/v1/auth/__init__.py
+++ backend/app/api/v1/auth/__init__.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 
-from api.v1.auth import login, register, callback
+from api.v1.auth import login, callback
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 router.include_router(login.router)
-router.include_router(register.router)
 router.include_router(callback.router)
