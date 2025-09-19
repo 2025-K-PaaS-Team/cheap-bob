b4e5b2c9063adae7a802822ec2a990011444baf7
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-19 17:19:41 +0900

    feat: chip in/active 상태 로직 구현

---
+++ frontend/src/components/common/Chips.tsx
@@ -0,0 +1,36 @@
+import type { SelectItem } from "@constant";
+
+interface ChipsProps {
+  chips: SelectItem[];
+  selected: Record<string, boolean>;
+  setSelected: React.Dispatch<React.SetStateAction<Record<string, boolean>>>;
+}
+
+const Chips = ({ chips, selected, setSelected }: ChipsProps) => {
+  return (
+    <div className="flex flex-wrap px-[20px]">
+      <div className="flex flex-wrap gap-[4px] items-center w-max">
+        {chips.map((chip, idx) => (
+          <div
+            className={`${
+              selected[chip.key]
+                ? "bg-[#222222] text-white"
+                : "bg-[#f0f0f0] text-black"
+            } rounded-[10px] px-[10px] py-[7px] text-center text-[12px] min-h-[28px] flex items-center whitespace-nowrap`}
+            key={idx}
+            onClick={() =>
+              setSelected((prev) => ({
+                ...prev,
+                [chip.key]: !prev[chip.key],
+              }))
+            }
+          >
+            {chip.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Chips;
--- frontend/src/pages/Customer/StoreList/StoreList.tsx
+++ frontend/src/pages/Customer/StoreList/StoreList.tsx
@@ -2,10 +2,18 @@ import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 import type { StoreResponseType } from "@interface";
 import { getStores } from "@services";
+import { Chips } from "@components/common";
+import { NutritionList } from "@constant";
 
 const StoreList = () => {
   const [stores, setStores] = useState<StoreResponseType[] | null>([]);
   const navigate = useNavigate();
+  const [selected, setSelected] = useState<Record<string, boolean>>(
+    NutritionList.reduce((acc, item) => {
+      acc[item.key] = false;
+      return acc;
+    }, {} as Record<string, boolean>)
+  );
 
   // get stores list
   const handleGetStores = async () => {
@@ -29,34 +37,41 @@ const StoreList = () => {
   };
 
   return (
-    <div className="flex flex-col gap-y-5 justify-center p-8">
-      {stores ? (
-        stores.map((store) => (
-          // store
-          <div
-            className="border-1 border-gray-400 shadow-md rounded-lg flex flex-col"
-            onClick={() => handleClickStore({ store_id: store.store_id })}
-            key={store.store_id}
-          >
-            {/* img */}
-            <img
-              src={
-                "https://mediahub.seoul.go.kr/uploads/mediahub/2024/01/ZmynWCLNyAQNfWWDfmSaLEByeOPGsaWZ.jpg"
-              }
-              alt="dummyStoreImg"
-            />
-            {/* desc */}
-            <div className="flex flex-row justify-between items-center p-4">
-              {/* store info left */}
-              <div>
-                <div className="font-bold">{store.store_name}</div>
+    <div className="flex flex-col">
+      <Chips
+        chips={NutritionList}
+        selected={selected}
+        setSelected={setSelected}
+      />
+      <div className="flex flex-col gap-y-5 justify-center p-8">
+        {stores ? (
+          stores.map((store) => (
+            // store
+            <div
+              className="border-1 border-gray-400 shadow-md rounded-lg flex flex-col"
+              onClick={() => handleClickStore({ store_id: store.store_id })}
+              key={store.store_id}
+            >
+              {/* img */}
+              <img
+                src={
+                  "https://mediahub.seoul.go.kr/uploads/mediahub/2024/01/ZmynWCLNyAQNfWWDfmSaLEByeOPGsaWZ.jpg"
+                }
+                alt="dummyStoreImg"
+              />
+              {/* desc */}
+              <div className="flex flex-row justify-between items-center p-4">
+                {/* store info left */}
+                <div>
+                  <div className="font-bold">{store.store_name}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <div>loading</div>
-      )}
+          ))
+        ) : (
+          <div>loading</div>
+        )}
+      </div>
     </div>
   );
   0;
