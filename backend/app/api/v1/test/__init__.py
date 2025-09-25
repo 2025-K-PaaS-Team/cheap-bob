bc63bcf692e16cb12ccf271ce998ca285580f5be
hyeonsang010716 <kyr76789@gmail.com>
2025-09-25 09:08:16 +0900

    mod: 주문 생성 Test API 삭제

--- backend/app/api/v1/test/__init__.py
+++ backend/app/api/v1/test/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
-from api.v1.test import payment, mongodb
+from api.v1.test import payment
 
 router = APIRouter(prefix="/test", tags=["Test"])
 
 router.include_router(payment.router)
-router.include_router(mongodb.router)
