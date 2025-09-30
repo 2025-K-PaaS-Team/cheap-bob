0991e078d67e53d6a83fa1e19af6f48c29a29d62
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-30 12:19:13 +0900

    refactor: dashboard 페이지 컴포넌트 분리

---
+++ frontend/src/components/seller/dashboard/index.ts
@@ -0,0 +1,1 @@
+export { default as NowStatus } from "./Now";
--- frontend/src/pages/Seller/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Seller/Dashboard/Dashboard.tsx
@@ -1,14 +1,10 @@
+import { NowStatus } from "@components/seller/dashboard";
+
 const Dashboard = () => {
   return (
     <div className="flex w-full flex-col">
       {/* now operating status */}
-      <div className="mx-[20px] flex flex-col gap-y-[3px] mt-[7px] bg-[#3A3A3A] rounded-[8px] py-[25px] px-[19px] text-white">
-        <div className="text-[24px]">
-          지금은 <span className="font-bold">운영중</span> 입니다.
-        </div>
-        <div className="text-[20px]">손님 픽업 시간까지 1시간 29분</div>
-        <div className="text-[16px]">영업 상태 변경 &gt;</div>
-      </div>
+      <NowStatus />
 
       {/* remaining package quantity */}
       <div className="mx-[20px] flex flex-col mt-[7px] bg-[#D9D9D9] rounded-[8px] py-[23px] px-[19px] text-black">
