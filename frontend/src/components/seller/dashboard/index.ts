f539f35d6f0e6e6fc1022dc0d7b9fe357c33227a
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-30 15:03:42 +0900

    feat: 매장 주소 변경 페이지 구현

--- frontend/src/components/seller/dashboard/index.ts
+++ frontend/src/components/seller/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { default as NowOpStatus } from "./NowOpStatus";
 export { default as RemainPkg } from "./RemainPkg";
 export { default as StoreManage } from "./StoreManage";
+export { default as PostalCode } from "./storeInfo/PostalCode";
--- frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreAddr.tsx
+++ frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreAddr.tsx
@@ -1,8 +1,29 @@
+import { CommonBtn } from "@components/common";
+import { PostalCode } from "@components/seller/dashboard";
+import { useNavigate } from "react-router";
+
 const ChangeStoreAddr = () => {
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="">
-      ChangeStoreAddr
-      <div></div>
+    <div className="mt-[80px] px-[20px] w-full">
+      {/* question */}
+      <div className="text-[24px]">
+        변경할 <span className="font-bold">매장 주소</span>를 <br /> 입력해
+        주세요.
+      </div>
+      {/* postal code */}
+      <PostalCode />
+
+      {/* 다음 */}
+      <CommonBtn
+        label="다음"
+        onClick={handleSubmit}
+        className="bg-black text-white"
+      />
     </div>
   );
 };
