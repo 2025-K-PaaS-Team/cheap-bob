0f5c4a6ad595ceaae6541f669944d8642b381da5
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-30 13:51:03 +0900

    feat: 매장 desc, num 변경 페이지 구현

--- frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreDesc.tsx
+++ frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreDesc.tsx
@@ -13,19 +13,23 @@ const ChangeStoreDesc = () => {
     <div className="mt-[80px] px-[20px] w-full">
       {/* question */}
       <div className="text-[24px]">
-        변경할 <span className="font-bold">매장 이름</span>을 <br /> 입력해
+        변경할 <span className="font-bold">매장 설명</span>을 <br /> 입력해
         주세요.
       </div>
       {/* input box */}
       <input
-        className="w-full h-[46px] text-center bg-[#D9D9D9] text-[16px] mt-[40px]"
-        placeholder="매장 이름을 입력해 주세요"
+        className="w-full h-[100px] text-center bg-[#D9D9D9] text-[16px] mt-[40px]"
+        placeholder="매장 설명을 입력해 주세요"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
 
       {/* 다음 */}
-      <CommonBtn label="다음" onClick={handleSubmit} />
+      <CommonBtn
+        label="다음"
+        onClick={handleSubmit}
+        className="bg-black text-white"
+      />
     </div>
   );
 };
