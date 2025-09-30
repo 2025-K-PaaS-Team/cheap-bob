b30583217c99f3c64ff3e2102ebace82db53e54a
gimgyuwon <gimgyuwon2@gmail.com>
2025-09-30 19:27:55 +0900

    feat: 패키지 할인율 변경 페이지 구현

--- frontend/src/pages/Seller/Dashboard/PackageInfo/ChangePackagePrice.tsx
+++ frontend/src/pages/Seller/Dashboard/PackageInfo/ChangePackagePrice.tsx
@@ -8,56 +8,55 @@ const ChangePackagePrice = () => {
   const handleSubmit = () => {
     navigate(-1);
   };
+  const discountRate = ["15", "30", "40", "50"];
 
   return (
     <div className="mt-[80px] px-[20px] w-full">
       {/* question */}
-      <div className="text-[24px]">
-        변경할 <span className="font-bold">매장 연락처</span>를 <br /> 입력해
-        주세요.
+      <div className="text-[24px]">패키지를 얼마에 판매할까요?</div>
+      <div className="text-[14px] font-bold">패지키 원가</div>
+      <div className="text-[14px]">
+        패키지를 구성하는 제품의 원가를 입력해 주세요.
       </div>
+
       {/* input box */}
-      <div className="mt-[40px] gap-y-[11px]">
-        <div className="text-[16px]">매장 전화번호 (*필수)</div>
+      <input
+        className="w-full h-[56px] text-center bg-[#D9D9D9] text-[16px] mt-[16px] mb-[40px]"
+        placeholder="제품의 원가를 입력해 주세요"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+
+      {/* discount rate */}
+      <div className="text-[14px] font-bold">패지키 할인율</div>
+      <div className="text-[14px]">패키지를 얼마나 할인할까요?</div>
+
+      {/* input box */}
+      <div className="flex flex-row items-center justify-center gap-x-[10px] mt-[16px]">
         <input
-          className="w-full h-[46px] text-center bg-[#D9D9D9] text-[16px]"
-          placeholder="매장 전화번호를 입력해 주세요"
+          className="w-[204px] h-[56px] text-center bg-[#D9D9D9] text-[16px]"
+          placeholder="할인율을 입력해 주세요"
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
+        <span className="text-[18px]">%</span>
+      </div>
+      {/* input assistance btn */}
+      <div className="flex flex-row gap-x-[7px] mt-[11px] mb-[40px] justify-center">
+        {discountRate.map((discount) => (
+          <div className="w-[52px] h-[24px] flex items-center justify-center text-center text-[13px] bg-[#d9d9d9] text-[#666666] rounded-[50px]">
+            {discount}%
+          </div>
+        ))}
       </div>
 
-      {/* other sns */}
-      <div className="mt-[39px] flex flex-col gap-y-[11px]">
-        <div className="text-[20px]">다른 SNS도 있나요?</div>
-        <div className="flex flex-row">
-          <div className="text-[14px] w-[97px] flex items-center">홈페이지</div>
-          <input
-            className="w-full h-[40px] text-center bg-[#D9D9D9] text-[16px]"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-row">
-          <div className="text-[14px] w-[97px] flex items-center">
-            인스타그램
-          </div>
-          <input
-            className="w-full h-[40px] text-center bg-[#D9D9D9] text-[16px]"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-row">
-          <div className="text-[14px] w-[97px] flex items-center">
-            X (Twitter)
-          </div>
-          <input
-            className="w-full h-[40px] text-center bg-[#D9D9D9] text-[16px]"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
+      {/* divider */}
+      <hr className="border-0 bg-black/10 h-[1px]" />
+
+      {/* sale price */}
+      <div className="flex flex-row justify-between items-center mt-[34px]">
+        <div className="font-bold text-[14px]">패키지 판매가</div>
+        <div className="font-bold text-[20px]">(원가*할인율)</div>
       </div>
 
       {/* 다음 */}
