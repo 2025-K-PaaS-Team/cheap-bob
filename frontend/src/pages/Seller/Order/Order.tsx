0b8ff1f5373bd0a43cd223ee34da54f0680ccebd
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-01 23:23:44 +0900

    feat: 주문 관리 summary 컴포넌트 구현

--- frontend/src/pages/Seller/Order/Order.tsx
+++ frontend/src/pages/Seller/Order/Order.tsx
@@ -5,6 +5,12 @@ const Order = () => {
     <>
       <Now />
       <StatusBar />
+
+      {/* summary */}
+      <div className="text-[14px] justify-between mx-[16px] flex flex-row my-[20px]">
+        <div>n건의 주문 내역이 있습니다.</div>
+        <div>주문 시간 순▽</div>
+      </div>
     </>
   );
 };
