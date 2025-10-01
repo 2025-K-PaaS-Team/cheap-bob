d22ad9201d5a0e5038e637b2d194ab8340e68df8
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-01 13:18:03 +0900

    feat: 매장 설명 등록 페이지 구현

--- frontend/src/components/seller/signup/StoreInfo/RegisterDesc.tsx
+++ frontend/src/components/seller/signup/StoreInfo/RegisterDesc.tsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { CommonBtn } from "@components/common";
+import { useRef, useState } from "react";
 
 const RegisterName = () => {
   const [value, setValue] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [previews, setPreviews] = useState<string[]>([]);
+
+  const handleClickUpload = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return;
+
+    const selectedFiles = Array.from(files).slice(0, 5 - previews.length); // 최대 5개
+    const urls = selectedFiles.map((file) => URL.createObjectURL(file));
+    setPreviews((prev) => [...prev, ...urls]);
+  };
+
+  const handleRemovePreview = (index: number) => {
+    setPreviews((prev) => prev.filter((_, i) => i !== index));
+  };
 
   return (
     <div className="mx-[20px] mt-[69px] flex flex-col gap-y-[11px]">
@@ -13,11 +33,51 @@ const RegisterName = () => {
 
       {/* input box */}
       <input
-        className="w-full h-[46px] text-center bg-[#D9D9D9] text-[16px] mt-[40px]"
-        placeholder="매장 이름을 입력해 주세요"
+        className="w-full h-[100px] text-center bg-[#D9D9D9] text-[16px] mt-[40px]"
+        placeholder="매장 설명을 입력해 주세요"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
+
+      {/* upload picture button */}
+      <div className="w-full flex justify-center">
+        <CommonBtn
+          label="(사진 업로드)"
+          notBottom
+          onClick={handleClickUpload}
+          className="border-1 border-[#999999] font-normal"
+        />
+      </div>
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        className="hidden"
+        onChange={handleFileChange}
+      />
+
+      {/* preview */}
+      {previews.length > 0 && (
+        <div className="grid grid-cols-4 gap-2 mt-[29px]">
+          {previews.map((src, idx) => (
+            <div key={idx} className="relative">
+              <img
+                src={src}
+                alt={`preview-${idx}`}
+                className="w-full h-[100px] object-contain"
+              />
+              <button
+                type="button"
+                onClick={() => handleRemovePreview(idx)}
+                className="absolute top-0 right-0 text-black text-xs w-5 h-5 flex items-center justify-center rounded-full"
+              >
+                X
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
