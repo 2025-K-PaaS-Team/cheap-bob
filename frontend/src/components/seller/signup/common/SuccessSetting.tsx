56df13e791ceacd55877d136b586e4c4952701a6
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-02 23:19:57 +0900

    feat: 설정 성공 페이지 조건 추가

--- frontend/src/components/seller/signup/common/SuccessSetting.tsx
+++ frontend/src/components/seller/signup/common/SuccessSetting.tsx
@@ -6,9 +6,21 @@ const SuccessSetting = ({ pageIdx, setPageIdx }: SellerSignupProps) => {
     setPageIdx(pageIdx + 1);
   };
 
+  const label = (pageIdx: number): string => {
+    if (pageIdx < 7) {
+      return "매장";
+    } else if (pageIdx < 11) {
+      return "영업";
+    } else {
+      return "패키지";
+    }
+  };
+
   return (
     <div className="min-h-screen mx-[37px] justify-center items-center flex flex-col gap-y-[11px]">
-      <div className="text-[24px]">매장 정보 설정을 완료했습니다!</div>
+      <div className="text-[24px]">
+        {label(pageIdx)} 정보 설정을 완료했습니다!
+      </div>
 
       <CommonBtn
         category="black"
