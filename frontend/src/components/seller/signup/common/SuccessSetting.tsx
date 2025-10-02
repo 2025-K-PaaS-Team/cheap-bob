0b2a6d9209ddf73111e360fadc80fe21352f640a
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-02 19:44:00 +0900

    feat: 매장 정보 설정 성공 화면 구현

--- frontend/src/components/seller/signup/common/SuccessSetting.tsx
+++ frontend/src/components/seller/signup/common/SuccessSetting.tsx
@@ -1,7 +1,20 @@
-const SuccessSetting = () => {
+import { CommonBtn } from "@components/common";
+import type { SellerSignupProps } from "@interface";
+
+const SuccessSetting = ({ pageIdx, setPageIdx }: SellerSignupProps) => {
+  const handleClickNext = () => {
+    setPageIdx(pageIdx + 1);
+  };
+
   return (
-    <div className="mx-[20px] mt-[69px] flex flex-col gap-y-[11px] text-center">
+    <div className="min-h-screen mx-[37px] justify-center items-center flex flex-col gap-y-[11px]">
       <div className="text-[24px]">매장 정보 설정을 완료했습니다!</div>
+
+      <CommonBtn
+        category="black"
+        label="확인"
+        onClick={() => handleClickNext()}
+      />
     </div>
   );
 };
