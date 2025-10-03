d2403ac1ce8df2df74243a7372a31934fa289057
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-03 21:25:41 +0900

    refactor: 구버전 전용 alias 삭제

--- frontend/src/pages/Common/Lab/index.ts
+++ frontend/src/pages/Common/Lab/index.ts
@@ -1,4 +1,1 @@
-export { default as Map } from "./CustomerMapLab";
 export { default as CustomerLab } from "./CustomerLab";
-export { default as SellerLab } from "./SellerLab";
-export { default as SignupLab } from "./SignupLab";
