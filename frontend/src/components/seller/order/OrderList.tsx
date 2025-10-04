5f559e7bf8e7ba8ce12300215c17d6e852e2e938
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-05 02:37:00 +0900

    feat: 주문 상태별 리스트 분리

--- frontend/src/components/seller/order/OrderList.tsx
+++ frontend/src/components/seller/order/OrderList.tsx
@@ -1,6 +1,11 @@
 import { CommonBtn } from "@components/common";
+import type { OrderBaseType } from "@interface";
 
-const OrderList = () => {
+interface OrderListProps {
+  orders: OrderBaseType[];
+}
+
+const OrderList = ({ orders }: OrderListProps) => {
   const handleClickCancel = () => {
     console.log("click cancel button");
   };
@@ -11,43 +16,46 @@ const OrderList = () => {
 
   return (
     <div className="flex flex-col mx-[16px] gap-y-[16px]">
-      <div className="flex flex-col bg-[#d9d9d9] rounded-[8px] py-[22px] px-[15px] gap-y-[16px]">
-        {/* first row */}
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex flex-row gap-x-[12px] items-center">
-            <div className="font-bold text-[16px]">13:22</div>
-            <div className="text-[20px]">
-              수량: <span className="font-bold">1개</span>
+      {!orders && (
+        <div className="flex flex-col bg-[#d9d9d9] rounded-[8px] py-[22px] px-[15px] gap-y-[16px]">
+          {/* first row */}
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row gap-x-[12px] items-center">
+              <div className="font-bold text-[16px]">13:22</div>
+              <div className="text-[20px]">
+                수량: <span className="font-bold">1개</span>
+              </div>
             </div>
+            <div className="text-[16px]">주문자 정보 더보기 &gt;</div>
           </div>
-          <div className="text-[16px]">주문자 정보 더보기 &gt;</div>
-        </div>
 
-        {/* second row */}
-        <div className="ml-[40px] bg-white rounded-[8px] flex flex-col py-[9.5px] px-[14px]">
-          <div className="text-[16px]">영양목표: 균형있게</div>
-          <div className="text-[16px]">선호 메뉴: 포케</div>
-          <div className="text-[16px]">알레르기 정보: 갑각류</div>
-        </div>
+          {/* second row */}
 
-        {/* third row - btn */}
-        <div className="flex flex-row ml-[40px]">
-          <CommonBtn
-            label="주문 취소"
-            onClick={() => handleClickCancel()}
-            notBottom
-            category="transparent"
-            width="w-1/3"
-          />
-          <CommonBtn
-            label="픽업 확정하기"
-            onClick={() => handleClickConfirm()}
-            notBottom
-            category="black"
-            width="w-2/3"
-          />
+          <div className="ml-[40px] bg-white rounded-[8px] flex flex-col py-[9.5px] px-[14px]">
+            <div className="text-[16px]">영양목표: 균형있게</div>
+            <div className="text-[16px]">선호 메뉴: 포케</div>
+            <div className="text-[16px]">알레르기 정보: 갑각류</div>
+          </div>
+
+          {/* third row - btn */}
+          <div className="flex flex-row ml-[40px]">
+            <CommonBtn
+              label="주문 취소"
+              onClick={() => handleClickCancel()}
+              notBottom
+              category="transparent"
+              width="w-1/3"
+            />
+            <CommonBtn
+              label="픽업 확정하기"
+              onClick={() => handleClickConfirm()}
+              notBottom
+              category="black"
+              width="w-2/3"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
