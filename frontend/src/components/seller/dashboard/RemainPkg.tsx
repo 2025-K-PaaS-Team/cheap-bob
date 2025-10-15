7e231068e97ccee796434a3cc4bebb5f7771387e
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-15 21:18:11 +0900

    feat: 잔여수량 디자인 적용

--- frontend/src/components/seller/dashboard/RemainPkg.tsx
+++ frontend/src/components/seller/dashboard/RemainPkg.tsx
@@ -40,29 +40,40 @@ const RemainPkg = ({
         현재 패키지 잔여 수량은 <br />{" "}
         <span className="font-bold">{remainPkg.current_stock}개</span> 입니다.
       </div>
-      <div className="grid grid-cols-4 text-center gap-x-[11px]">
+      <div className="flex flex-row text-center justify-between">
+        {/* 기본값 */}
         <div className="flex flex-col py-[14px]">
           <div className="text-[14px]">판매 기본값</div>
           <div className="text-[24px]">{remainPkg.initial_stock}</div>
         </div>
-        <div className="flex flex-col py-[14px]">
-          <div className="text-[14px]">- 주문 수량</div>
+
+        {/* 마이너스 */}
+        <img src="/icon/minus.svg" alt="minusIcon" className="pt-[15px]" />
+
+        {/* 주문 수량 */}
+        <div className="flex flex-col py-[14px] ">
+          <div className="text-[14px]">주문 수량</div>
           <div className="text-[24px]">{remainPkg.purchased_stock}</div>
         </div>
-        <div className="relative py-[14px] flex flex-row col-span-2 bg-main-pale rounded-[8px] justify-center">
+
+        {/* 플러스 */}
+        <img src="/icon/plus.svg" alt="plusIcon" className="pt-[15px]" />
+
+        {/* 일일조정값 */}
+        <div className="relative py-[14px] w-[115px] flex flex-row  bg-main-pale rounded-[8px] justify-center">
           <div className="flex flex-col justify-center">
-            <div className="text-[14px]">+ 일일 조정값</div>
+            <div className="text-[14px]">일일 조정값</div>
             <div className="text-[24px]">{remainPkg.adjustment_stock}</div>
           </div>
-          <div className="absolute right-[10px] top-[31px] flex flex-col justify-center gap-y-[4px]">
+          <div className="absolute right-[5px] top-[35px] flex flex-col justify-center gap-y-[4px]">
             <div
-              className="bg-custom-white w-[20px] aspect-square flex items-center justify-center text-[10px] rounded-full"
+              className="bg-custom-white text-main-deep w-[20px] aspect-square flex items-center justify-center text-[10px] rounded-full"
               onClick={() => handleIncreaseStock()}
             >
               ▲
             </div>
             <div
-              className="bg-custom-white w-[20px] aspect-square flex items-center justify-center text-[10px] rounded-full"
+              className="bg-custom-white text-main-deep w-[20px] aspect-square flex items-center justify-center text-[10px] rounded-full"
               onClick={() => handleDecreaseStock()}
             >
               ▼
