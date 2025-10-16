73c65959af24ce132f66ac68a3b9dbd9767b0a20
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-17 01:23:51 +0900

    feat: Store list 페이지 디자인 적용

--- frontend/src/components/customer/storeList/StoreBox.tsx
+++ frontend/src/components/customer/storeList/StoreBox.tsx
@@ -20,7 +20,7 @@ const StoreBox = ({ stores, onToggleFavorite }: StoreBoxProps) => {
       {stores.map((store) => (
         <div
           key={store.store_id}
-          className="flex flex-col h-[212px] relative"
+          className="flex flex-col h-[235px] relative"
           onClick={() => handleClickStore(store)}
         >
           {/* favor */}
@@ -32,22 +32,14 @@ const StoreBox = ({ stores, onToggleFavorite }: StoreBoxProps) => {
             }}
           >
             {store.is_favorite ? (
-              <img
-                src="/icon/heartFull.svg"
-                alt="FavoriteStore"
-                className="w-5 h-5"
-              />
+              <img src="/icon/heartFull.svg" alt="FavoriteStore" width="24px" />
             ) : (
-              <img
-                src="/icon/heart.svg"
-                alt="FavoriteStore"
-                className="w-5 h-5"
-              />
+              <img src="/icon/heart.svg" alt="FavoriteStore" width="24px" />
             )}
           </div>
 
           {/* store image */}
-          <div className="h-[125px] rounded-t overflow-hidden relative">
+          <div className="h-[131px] rounded-t overflow-hidden relative">
             {/* now open */}
             <div className="bg-custom-white rounded-lg absolute bottom-2 left-2 z-10 py-[4px] px-[10px]">
               {store.operation_times.find((dow) => dow.day_of_week === todayDow)
@@ -64,17 +56,16 @@ const StoreBox = ({ stores, onToggleFavorite }: StoreBoxProps) => {
 
           {/* store info*/}
           <div className="bg-white rounded-b shadow p-[15px] flex flex-1 flex-col gap-y-[7px]">
+            {/* first row - store name */}
             <div className="flex flex-row justify-between h-full items-end">
               <div className="text-[15px] font-bold">{store.store_name}</div>
-              <div className="bg-[#BFBFBF] p-[5px] rounded text-[10px]">
-                {store.products[0].price} 원
-              </div>
             </div>
 
+            {/* second row - pickup time */}
             <div className="flex flex-row justify-between h-full">
-              <div className="flex flex-row gap-x-[12px] items-center">
-                <div className="text-[12px]">픽업 시간</div>
-                <div className="bg-[#BFBFBF] p-[5px] rounded text-[12px]">
+              <div className="flex flex-row gap-x-[12px] items-center font-bold tagFont">
+                <div className="">픽업 시간</div>
+                <div className="">
                   {store.operation_times
                     .find((dow) => dow.day_of_week === todayDow)
                     ?.pickup_start_time.slice(0, 5)}
@@ -84,9 +75,39 @@ const StoreBox = ({ stores, onToggleFavorite }: StoreBoxProps) => {
                     ?.pickup_end_time.slice(0, 5)}
                 </div>
               </div>
-              <div className="bg-[#BFBFBF] p-[5px] rounded text-[11px]">
-                {(store.products[0].price * store.products[0].sale) / 100}원
+            </div>
+
+            {/* third row - pkg remian qty */}
+            <div className="flex flex-row justify-between tagFont">
+              {/* remain qty */}
+              {store.products[0].current_stock == 0 ? (
+                <div className="bg-[#E7E7E7] rounded-sm py-[5.5px] px-[10px]">
+                  패키지 <span className="text-sub-orange font-bold">품절</span>
+                </div>
+              ) : (
+                <div className="bg-[#E7E7E7] rounded-sm py-[5.5px] px-[10px]">
+                  패키지{" "}
+                  <span className="text-main-deep font-bold">
+                    {store.products[0].current_stock}개 남음
+                  </span>
+                </div>
+              )}
+            </div>
+
+            {/* absolute position - price */}
+            <div className="absolute right-3 bottom-3 text-end">
+              <div className="tagFont text-[#9D9D9D] line-through">
+                {store.products[0].price}
               </div>
+              <h1
+                className={`${
+                  store.products[0].current_stock == 0
+                    ? "text-[#9d9d9d]"
+                    : "text-sub-orange"
+                }`}
+              >
+                {(store.products[0].price * store.products[0].sale) / 100}원
+              </h1>
             </div>
           </div>
         </div>
