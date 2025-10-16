a7c473bfc46133ca680f730b78550562cb31fc25
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-16 17:25:19 +0900

    feat: order 현재 상태 디자인 수정

--- frontend/src/components/seller/order/Now.tsx
+++ frontend/src/components/seller/order/Now.tsx
@@ -1,13 +1,13 @@
 const NowStatus = () => {
   return (
-    <div className="flex flex-col mx-[20px] my-[15px] gap-y-[15px]">
-      <div className="flex flex-col bg-main-500 rounded-sm p-[17px]">
-        <div className="text-[24px]">
-          지금은 <span className="font-bold">픽업 시간</span> 입니다
+    <div className="flex flex-col px-[20px] py-[16px] gap-y-[8px] bg-[#393939]">
+      <div className="flex flex-col bg-main-500 rounded-sm">
+        <div className="titleFont text-white">
+          지금은 <span className="font-bold text-main-deep">픽업 시간</span>{" "}
+          입니다
         </div>
-        <div className="text-[18px]">픽업 마감 시간까지 1시간 29분</div>
       </div>
-      <div className="text-[14px] flex flex-row justify-between">
+      <div className="hintFont text-[#9D9D9D] flex flex-row justify-between">
         <div>마지막 업데이트: 13시 25분</div>
         <div>새로고침 O</div>
       </div>
