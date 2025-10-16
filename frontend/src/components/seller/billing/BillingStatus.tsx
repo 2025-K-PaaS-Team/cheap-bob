6d4c3baa14b90afb1789f1fb6b1ec2969fc93bb3
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-17 00:14:38 +0900

    feat: 정산 내역 확인 기간 및 상태 구현

---
+++ frontend/src/components/seller/billing/BillingStatus.tsx
@@ -0,0 +1,30 @@
+import { BillingOption } from "@constant";
+
+interface BillingStatusProps {
+  nowStatus: string;
+  setNowStatus: (nowStatus: string) => void;
+}
+
+const BillingStatus = ({ nowStatus, setNowStatus }: BillingStatusProps) => {
+  return (
+    <div className="flex flex-row items-center gap-x-[10px]">
+      <div className="font-bold mr-auto">상태</div>
+
+      {BillingOption.map((status, idx) => (
+        <div
+          key={idx}
+          onClick={() => setNowStatus(status.key)}
+          className={`${
+            nowStatus == status.key
+              ? "bg-main-pale border border-main-deep text-main-deep"
+              : ""
+          } w-fit py-[8px] px-[16px] rounded tagFont`}
+        >
+          {status.title}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BillingStatus;
---
+++ frontend/src/components/seller/billing/index.ts
@@ -0,0 +1,1 @@
+export { default as BillingStatus } from "./BillingStatus";
