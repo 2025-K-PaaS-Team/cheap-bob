ccf3e562f320b603d8422edc718bfd3a9e515a94
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-20 17:34:42 +0900

    feat: 매장 주소 변경 공통 컴포넌트 구현

--- frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreAddr.tsx
+++ frontend/src/pages/Seller/Dashboard/StoreInfo/ChangeStoreAddr.tsx
@@ -1,11 +1,10 @@
 import { CommonBtn, CommonModal } from "@components/common";
 import { useEffect, useState } from "react";
-import { PostalCode } from "@components/seller/dashboard";
+import { NearStation, PostalCode } from "@components/seller/dashboard";
 import { useNavigate } from "react-router";
 import { GetStoreDetail, UpdateStoreAddr } from "@services";
 import { formatErrMsg } from "@utils";
 import type { AddressInfoType } from "@interface";
-import { NearStationList } from "@constant";
 
 const ChangeStoreAddr = () => {
   const initialAddr: AddressInfoType = {
@@ -24,10 +23,8 @@ const ChangeStoreAddr = () => {
   const navigate = useNavigate();
   const [addr, setAddr] = useState<AddressInfoType>(initialAddr);
 
-  // 모달들
   const [showModal, setShowModal] = useState<boolean>(false);
   const [modalMsg, setModalMsg] = useState("");
-  const [openStation, setOpenStation] = useState<boolean>(false);
 
   const [selectedStation, setSelectStation] = useState<string>("");
   const [stationTime, setStationTime] = useState<string>("");
@@ -46,11 +43,6 @@ const ChangeStoreAddr = () => {
     };
     handleGetStore();
   }, []);
-
-  const handleStationTimeChange = (raw: string) => {
-    const onlyNum = raw.replace(/\D/g, "").slice(0, 2);
-    setStationTime(onlyNum);
-  };
 
   const validateAddr = (a: AddressInfoType) => {
     if (!a.address || !a.postal_code) {
@@ -106,46 +98,13 @@ const ChangeStoreAddr = () => {
           }
         />
 
-        <div className="flex flex-col gap-y-[20px]">
-          <h2>가장 가까운 역은 어디인가요?</h2>
-
-          {selectedStation ? (
-            <div className="flex flex-row gap-x-[5px] items-center hintFont">
-              <div className="max-w-[122px]">
-                <CommonBtn
-                  notBottom
-                  onClick={() => setOpenStation(true)}
-                  label={selectedStation}
-                  category="pale-green"
-                  className="w-fit h-fit px-[16px] py-[8px] tagFont rounded-sm"
-                />
-              </div>
-              <div>에서 도보로</div>
-              <div className="border-b border-[#393939] w-[30px]">
-                <input
-                  type="text"
-                  value={stationTime}
-                  onChange={(e) => handleStationTimeChange(e.target.value)}
-                  className="w-full text-center"
-                  maxLength={2}
-                  inputMode="numeric"
-                  pattern="[0-9]*"
-                />
-              </div>
-              <div>분 걸려요.</div>
-            </div>
-          ) : (
-            <div className="max-w-[122px]">
-              <CommonBtn
-                notBottom
-                label="역을 선택해주세요."
-                category="pale-green"
-                className="w-fit h-fit p-[8px] tagFont rounded-sm"
-                onClick={() => setOpenStation(true)}
-              />
-            </div>
-          )}
-        </div>
+        <NearStation
+          setAddr={setAddr}
+          setStationTime={setStationTime}
+          stationTime={stationTime}
+          selectedStation={selectedStation}
+          setSelectStation={setSelectStation}
+        />
       </div>
 
       {/* save */}
@@ -155,43 +114,6 @@ const ChangeStoreAddr = () => {
         notBottom
         category="green"
       />
-
-      {/* 역 선택 모달 */}
-      {openStation && (
-        <CommonModal
-          confirmLabel="확인"
-          onConfirmClick={() => {
-            setOpenStation(false);
-            setAddr((prev) => ({
-              ...prev,
-              nearest_station: selectedStation || "",
-              walking_time: stationTime ? Number(stationTime) : 0,
-            }));
-          }}
-          onCancelClick={() => setOpenStation(false)}
-          category="green"
-          className="text-start"
-        >
-          <div className="flex flex-col max-h-[150px] mt-[5px] overflow-y-auto bodyFont gap-y-[10px]">
-            <div className="font-bold">역을 선택해주세요</div>
-            <div className="flex flex-col">
-              {NearStationList.map((station, idx) => (
-                <div
-                  key={idx}
-                  onClick={() => setSelectStation(station)}
-                  className={`min-h-[36px] p-1 cursor-pointer ${
-                    station === selectedStation
-                      ? "font-bold bg-custom-white"
-                      : ""
-                  }`}
-                >
-                  {station}
-                </div>
-              ))}
-            </div>
-          </div>
-        </CommonModal>
-      )}
 
       {showModal && (
         <CommonModal
