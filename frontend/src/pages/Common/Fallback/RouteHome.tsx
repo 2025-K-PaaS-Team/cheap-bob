4ff5129c3b6692aeb9c524fa2c93245042b2387e
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-24 23:04:39 +0900

    feat: 루트 홈 navigate추가

--- frontend/src/pages/Common/Fallback/RouteHome.tsx
+++ frontend/src/pages/Common/Fallback/RouteHome.tsx
@@ -1,6 +1,6 @@
 import { CommonBtn, HomeSwiper } from "@components/common";
 import { homeSwiperMap } from "@constant";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,6 +9,10 @@ import { Pagination } from "swiper/modules";
 
 const RouteHome = () => {
   const navigate = useNavigate();
+  const loginRole = localStorage.getItem("loginRole");
+
+  if (loginRole === "seller") return <Navigate to="/s" replace />;
+  if (loginRole === "customer") return <Navigate to="/c" replace />;
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center text-center my-[20px] mx-auto w-[calc(100%-40px)] gap-y-[28px]">
