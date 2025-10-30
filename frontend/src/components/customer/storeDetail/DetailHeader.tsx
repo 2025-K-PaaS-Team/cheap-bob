6ef486de5784171902a1fdf90791efd76d89acd3
gimgyuwon <gimgyuwon2@gmail.com>
2025-10-30 18:31:46 +0900

    fix: 가게 상세 헤더 grid를 flex로 조정

--- frontend/src/components/customer/storeDetail/DetailHeader.tsx
+++ frontend/src/components/customer/storeDetail/DetailHeader.tsx
@@ -10,19 +10,20 @@ const DetailHeader = ({ name }: DetailHeaderProps) => {
   }
 
   return (
-    <div className="min-h-[60px] mt-[10px] px-[20px] grid grid-cols-3 items-center">
+    <div className="min-h-[60px] mt-[10px] px-[20px] flex flex-row items-center">
       {/* left */}
-      <img
-        src="/icon/before.svg"
-        alt="beforeArrowIcon"
-        onClick={() => navigate(-1)}
-      />
+      <div className="w-5">
+        <img
+          src="/icon/before.svg"
+          alt="beforeArrowIcon"
+          onClick={() => navigate(-1)}
+        />
+      </div>
 
       {/* center */}
-      <div className="font-bold text-[15px] text-center">{name || ""}</div>
-
-      {/* right */}
-      <div />
+      <div className="font-bold text-[15px] text-center w-full pr-5">
+        {name || ""}
+      </div>
     </div>
   );
 };
